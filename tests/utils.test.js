d99c321eb12320628cbbb9462d138f45b2902f93
Adam Oliveras <theadamoliveras@gmail.com>
2019-08-05 20:10:01 -0700

    created utils.test.js

---
+++ tests/utils.test.js
@@ -0,0 +1,13 @@
+const {
+  isUniform,
+  parseIntBase10,
+  doesNotInclude,
+  satisfies,
+  columns,
+  diagonals,
+  pad,
+} = require('../lib/utils');
+
+describe('isUniform', () => {
+  it.todo('should ');
+});
